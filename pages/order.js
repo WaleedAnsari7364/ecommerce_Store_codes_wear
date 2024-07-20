d9faca4029fc74d38173dbe91ef62ce9f55ebce3
waleed <waleedansari7364@gmail.com>
2024-07-20 21:21:23 +0500

    Navbar and order page adjustment

--- pages/order.js
+++ pages/order.js
@@ -3,9 +3,7 @@ import { useRouter } from "next/router";
 import Order from "@/models/Order";
 import mongoose from "mongoose";
 
-const MyOrder = ({order,clearCart}) => {
-  // const router=useRouter()
-  // const {id}=router.query
+const MyOrder = ({order}) => {
   const products=order.products
   const [date, setDate] = useState()
   useEffect(() => {
@@ -14,10 +12,10 @@ const MyOrder = ({order,clearCart}) => {
   }, [])
   
   return (
-    <section className="text-gray-600 body-font overflow-hidden">
+    <section className="text-gray-600 body-font overflow-hidden min-h-screen">
       <div className="container px-5 py-24 mx-auto">
-        <div className="lg:w-4/5 mx-auto flex flex-wrap">
-          <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+        <div className="mx-auto flex flex-wrap">
+          <div className="w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
             <h2 className="text-sm title-font text-gray-500 tracking-widest">
               CODESWEAR
             </h2>
@@ -32,17 +30,18 @@ const MyOrder = ({order,clearCart}) => {
               Your Payment Status is : <b>{order.status}</b>
             </p>
             <br />
+            <br />
             <div class="flex mb-4">
-          <a class="flex-grow text-center  py-2 text-lg px-1 ">Item Description</a>
-          <a class="flex-grow text-center  py-2 text-lg px-1">Quantity</a>
-          <a class="flex-grow text-center  py-2 text-lg px-1">Item Total</a>
+          <a class="flex-grow py-2 text-lg w-1/3">Item Description</a>
+          <a class="flex-grow py-2 text-lg w-1/3">Quantity</a>
+          <a class="flex-grow py-2 text-lg w-1/3">Item Total</a>
            </div>
 
            {Object.keys(products).map((key)=>{
               return <div key={key} className="flex border-t border-gray-200 py-2">
-              <span className="text-gray-500">{products[key].name} ({products[key].size}/{products[key].variant})</span>
-              <span className="m-auto text-gray-900">{products[key].qty}</span>
-              <span className="m-auto text-gray-900">{products[key].price} X {products[key].qty} = {products[key].price * products[key].qty}</span>
+              <span className="text-gray-500 w-1/3">{products[key].name} ({products[key].size}/{products[key].variant})</span>
+              <span className="m-auto text-gray-900 w-1/3">{products[key].qty}</span>
+              <span className="m-auto text-gray-900 w-1/3">{products[key].price} X {products[key].qty} = {products[key].price * products[key].qty}</span>
             </div>})}
 
             <div className="flex">
@@ -54,11 +53,6 @@ const MyOrder = ({order,clearCart}) => {
               </button>
             </div>
           </div>
-          <img
-            alt="ecommerce"
-            className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-            src="/order.jpg"
-          />
         </div>
       </div>
     </section>
